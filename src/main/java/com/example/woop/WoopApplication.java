947b3bc3c7ef0618ccad92860a517323608c5f79
unknown <woneee99@gmail.com>
2022-06-25 17:03:29 +0900

    chore: project setting

---
+++ src/main/java/com/example/woop/WoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.woop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/woop/WoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
